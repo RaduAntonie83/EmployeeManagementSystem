222fc9e5061046641a35839ed75d96500c26000a
antoniavs <127041555+antoniavs@users.noreply.github.com>
2024-01-03 16:29:31 +0200

    Created an Edit Employee page

---
+++ src/main/java/com/employee/employeemanagementsystem/EditEmployee.java
@@ -0,0 +1,28 @@
+package com.employee.employeemanagementsystem;
+
+import jakarta.servlet.*;
+import jakarta.servlet.http.*;
+import jakarta.servlet.annotation.*;
+
+import java.io.IOException;
+
+@WebServlet(name = "EditEmployee", value = "/EditEmployee")
+public class EditEmployee extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse
+            response) throws ServletException, IOException {
+        request.getRequestDispatcher("/WEB-INF/pages/editEmployee.jsp").forward(request,response);
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest request,
+                          HttpServletResponse response) throws ServletException, IOException {
+        String name = request.getParameter("name");
+        String address = request.getParameter("address");
+        double salary = Double.parseDouble(request.getParameter("salary"));
+        String employeeClass = request.getParameter("employeeClass");
+        int hours = Integer.parseInt(request.getParameter("hours"));
+        String gender = request.getParameter("gender");
+        String birthdate = request.getParameter("birthdate");
+    }
+}
