bf0f24c50925f917905c203c2f312bdf210dd381
RaduAntonie86 <128983932+RaduAntonie86@users.noreply.github.com>
2024-01-05 20:02:49 +0200

    Revert ""Added Associate/Executive class""
    This reverts commit 3f957d69c05deae59e5e5df1eef6b2e0c0aa5187.

--- src/main/java/com/employee/employeemanagementsystem/entities/Associate.java
+++
@@ -1,7 +0,0 @@
-package com.employee.employeemanagementsystem.entities;
-
-import com.employee.employeemanagementsystem.Bonus;
-
-public class Associate extends Employee{
-    private Bonus bonus;
-}
--- src/main/java/com/employee/employeemanagementsystem/entities/Executive.java
+++
@@ -1,9 +0,0 @@
-package com.employee.employeemanagementsystem.entities;
-
-import com.employee.employeemanagementsystem.Bonus;
-import jakarta.persistence.Basic;
-
-public class Executive extends Employee{
-    private int numberOfShares;
-    private Bonus bonus;
-}
